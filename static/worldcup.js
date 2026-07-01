b8febbcb3eb79eb4334c5069ea6d3d0de121e997
muxammadjontojiyev-star <muxammadjontojiyev@gmail.com>
2026-07-01 12:46:40 +0500

    Add files via upload

--- static/worldcup.js
+++ static/worldcup.js
@@ -132,7 +132,7 @@ function renderWorldCup() {
 
   root.innerHTML = `
     <div class="wc-banner">
-      <img src="worldcup-banner.jpg?v=20260628z" alt="World Cup 2026" class="wc-banner-img" />
+      <img src="worldcup-banner.jpg?v=20260628a3" alt="World Cup 2026" class="wc-banner-img" />
     </div>
     <div class="wc-header">
       <button class="wc-back" id="wc-back-btn">
